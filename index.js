2ed33084f83da350beb1cc5aeb61b92e264de25d
TheExiledCat <kazekage2002@hotmail.com>
2023-04-13 09:03:59 +0200

    Changed output to Api generwtor

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const https = require('https')
 const app = express()
 const port = 3000;
 app.use(express.json());
-
+let randomName="";
 app.get('/', (req, res) => {
   let data = req.body;
   console.log(req.query);
@@ -10,7 +11,33 @@ app.get('/', (req, res) => {
   res.send("I Love " + req.query.name);
   res.end();
 })
+ app.get('/:id', (req, res) => {
+  res.send(randomName);
+cycle() 
+  res.end();
+})
+async function cycle()
+{
+  await https
+  .get(`https://randomuser.me/api/`, resp => {
+    let data = "";
 
+    // A chunk of data has been recieved.
+    resp.on("data", chunk => {
+      data += chunk;
+    });
+
+    // The whole response has been received. Print out the result.
+    resp.on("end", () => {
+      let url = JSON.parse(data).message;
+      randomName=JSON.parse(data).results[0].name.first
+    });
+  })
+  .on("error", err => {
+    console.log("Error: " + err.message);
+  });
+}
+cycle()
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
